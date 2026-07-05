22b4ebf19e7cd06f3ca2d7a5358de6b83615faba
j0hv6901-sudo <j0hv6901@gmail.com>
2026-07-05 20:57:54 +0530

    Add sidebar HTML structure to sidebar.js

---
+++ assets/sidebar.js
@@ -0,0 +1,11 @@
+document.getElementById("sidebar").innerHTML = `
+<div class="sidebar">
+    <h2 style="padding:15px;">Hotel Admin</h2>
+    <a href="dashboard.html">Dashboard</a><br><br>
+    <a href="bookings.html">Bookings</a><br><br>
+    <a href="rooms.html">Rooms</a><br><br>
+    <a href="hall.html">Hall Booking</a><br><br>
+    <a href="offers.html">Offers</a><br><br>
+    <a href="settings.html">Settings</a>
+</div>
+`;
